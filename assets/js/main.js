e4d4acec532742934191fcfc4ffe6225cb4dd1f1
kazu <nakazato@zig-zag.co.jp>
2026-04-25 15:37:49 +0900

    create

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// Mobile nav toggle
+(function () {
+  var toggle = document.querySelector('.nav-toggle');
+  var links  = document.querySelector('.nav-links');
+  if (!toggle || !links) return;
+
+  toggle.addEventListener('click', function () {
+    var open = links.classList.toggle('open');
+    toggle.setAttribute('aria-expanded', open);
+  });
+
+  // Close menu on link click
+  links.querySelectorAll('a').forEach(function (a) {
+    a.addEventListener('click', function () {
+      links.classList.remove('open');
+      toggle.setAttribute('aria-expanded', 'false');
+    });
+  });
+}());
